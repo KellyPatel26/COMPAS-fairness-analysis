4f33cdc98dcdb3ecc7357cdb94c906f0c300a77e
Anita de Mello Koch <demellokochanita@gmail.com>
2023-04-18 15:54:14 -0400

    explainer added

---
+++ code/explainer.py
@@ -0,0 +1,54 @@
+import captum
+import numpy as np
+
+from captum.attr import LimeBase, ShapleyValueSampling, FeaturePermutation, GradientShap, IntegratedGradients
+from captum._utils.models.linear_model import SkLearnLinearModel
+from captum.attr._core.lime import get_exp_kernel_similarity_function
+
+class Explainer():
+    def __init__(self,
+                 model):
+        self.model = model
+        
+        self.lime_ex = LimeBase(
+            self.model,
+            SkLearnLinearModel(),
+            get_exp_kernel_similarity_function('euclidean')
+        )
+        
+        self.shapley_ex = ShapleyValueSampling(
+            self.model
+        )
+        
+        self.dice_ex = FeaturePermutation(
+            self.model
+        )
+        
+        self.grad_shap_ex = GradientShap(
+            self.model
+        )
+        
+    def lime(self, input, target):
+        return self.lime_ex.attribute(input, target=target)
+
+    def shapley(self, input, target):
+        return self.shapley_ex.attribute(input, target=target)
+
+    def dice(self, input, target):
+        return self.dice_ex(input, target=target)
+
+    def grad_shap(self, input, target):
+        return self.grad_shap_ex(
+            input,
+            target=target,
+            n_samples=20
+        )
+
+
+
+
+
+
+
+
+
